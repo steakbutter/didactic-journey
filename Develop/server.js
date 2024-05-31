be977052e8a8e26f16843b3f3edcfa5ea4af7800
Carlos Jorge Gonzalez Perales <carlosgzzperales@gmail.com>
2024-05-31 15:13:40 -0600

    added the GET route for index.html file

--- Develop/server.js
+++ Develop/server.js
@@ -13,5 +13,10 @@ app.use(express.urlencoded({ extended: true}));
 
 // GET request for notes.html
 app.get('/notes', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// GET request for index.html file
+app.get('*', (req, res) => 
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
