5f748667ddb1d773d383bef30c7d1e5e0a6d2765
Carlos Jorge Gonzalez Perales <carlosgzzperales@gmail.com>
2024-05-31 15:07:10 -0600

    Added middleware for parsing app json and urlencoded data

--- Develop/server.js
+++ Develop/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const path = require('path');
 const fs = require('fs');
 
 // Define the PORT number
 const PORT = 3001;
 // call Express
 const app = express();
+
+// Middleware for parsing application/json and urlencoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 
