6dce3e93bec33d39c4a0eac5137adaee81c658f7
Carlos Jorge Gonzalez Perales <carlosgzzperales@gmail.com>
2024-05-31 18:21:02 -0600

    Added a GET request for notes

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const notes = require('./db/db.json')
 // Helper method for generating unique ids
 const uuid = require('./helpers/uuid');
 // Define the PORT number
@@ -22,6 +23,11 @@ app.get('*', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+//GET request to read db.json file
+app.get('/api/notes', (req, res => {
+    console.info(`${req.method} request received to get notes`);
+    return res.json(notes);
+}));
 
 
 
