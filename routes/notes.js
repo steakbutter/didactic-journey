a3cd43fd4f2136c538f8bf2cf15a52158bd06cb9
Carlos Jorge Gonzalez Perales <carlosgzzperales@gmail.com>
2024-06-06 18:20:14 -0600

    Added the GET route for api/notes to read the db.json file

--- routes/notes.js
+++ routes/notes.js
@@ -3,4 +3,11 @@ const notes = require('express').Router();
 const uuid = require("../helpers/uuid");
 
 // function to help read and write JSON file 
-const { readFromFile, readAndAppend} = require
+const { readFromFile, readAndAppend} = require('../helpers/fsUtils');
+
+// GET route for retrieving all notes
+notes.get('/api/notes', (req, res) => {
+    console.info(`${req.method} request received for notes`);
+
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
