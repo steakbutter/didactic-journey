104a2b27a287b3b083109d4f3feea4a46804275e
Carlos Jorge Gonzalez Perales <carlosgzzperales@gmail.com>
2024-06-06 18:04:37 -0600

    Added fsutil file to add read and write function

--- routes/notes.js
+++ routes/notes.js
@@ -1,3 +1,6 @@
 const notes = require('express').Router();
 
 const uuid = require("../helpers/uuid");
+
+// function to help read and write JSON file 
+const { readFromFile, readAndAppend} = require
