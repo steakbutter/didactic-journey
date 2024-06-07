7dd3cfc0919ec8c0874582722960b90fa20e7b89
Carlos Jorge Gonzalez Perales <carlosgzzperales@gmail.com>
2024-06-06 18:30:54 -0600

    create the index.js with routes in routes folder

---
+++ routes/index.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+
+// import routes containing files
+const notesRouter = require('./routes/notes');
+
+router.use('/api/notes', notesRouter);
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const notes = require('./db/db.json');
+
 
 // Helper method for generating unique ids
 const uuid = require('./helpers/uuid');
